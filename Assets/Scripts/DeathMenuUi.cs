1cd869503169050261e03ed4e9b81f9ae33bf4d9
Kamil <kamil.antos@qysystems.pro>
2024-06-07 14:08:12 +0200

    Added ui updates

--- Assets/Scripts/DeathMenuUi.cs
+++ Assets/Scripts/DeathMenuUi.cs
@@ -1,10 +1,23 @@
-﻿using Enums;
+﻿using System;
+using Enums;
 using UnityEngine;
 
 public class DeathMenuUi : MonoBehaviour
 {
+    [SerializeField] private GameObject needsUi;
+
+    private void Awake()
+    {
+        needsUi.SetActive(false);
+    }
+
     public void ReturnToMenu()
     {
         GameManager.Instance.LoadScene(ESceneType.Menu);
-    }       
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
